cc04bab96f4494f4c6909c73aa3123c08eb9c5a5
Stanimir Stoev <stoevs@gmail.com>
2024-01-22 16:14:59 +0200

    Attempt at functions. Needs BLAZE plan

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,13 +1,13 @@
 export const environment = {
 	production: true,
 	firebaseConfig: {
-		apiKey: "",
-		authDomain: "",
-		databaseURL: "",
-		projectId: "",
-		storageBucket: "",
-		messagingSenderId: "",
-		appId: "",
-		measurementId: ""
-	},
+		apiKey: "AIzaSyCDgJkLiwZTqf6jLp_AqY0HqSRuAfhdar0",
+		authDomain: "okevents-76a7c.firebaseapp.com",
+		databaseURL: "https://okevents-76a7c-default-rtdb.europe-west1.firebasedatabase.app",
+		projectId: "okevents-76a7c",
+		storageBucket: "okevents-76a7c.appspot.com",
+		messagingSenderId: "705007956755",
+		appId: "1:705007956755:web:b124fb79275953bc00a0e6",
+		measurementId: "G-TT7KQB6JD6"
+	  },
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,15 +5,15 @@
 export const environment = {
 	production: false,
 	firebaseConfig: {
-		apiKey: "",
-		authDomain: "",
-		databaseURL: "",
-		projectId: "",
-		storageBucket: "",
-		messagingSenderId: "",
-		appId: "",
-		measurementId: ""
-	},
+		apiKey: "AIzaSyCDgJkLiwZTqf6jLp_AqY0HqSRuAfhdar0",
+		authDomain: "okevents-76a7c.firebaseapp.com",
+		databaseURL: "https://okevents-76a7c-default-rtdb.europe-west1.firebasedatabase.app",
+		projectId: "okevents-76a7c",
+		storageBucket: "okevents-76a7c.appspot.com",
+		messagingSenderId: "705007956755",
+		appId: "1:705007956755:web:b124fb79275953bc00a0e6",
+		measurementId: "G-TT7KQB6JD6"
+	  },
 };
 
 /*
